2b10f3768eec4bfe8036b160e33a46a6f782a28e
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2024-01-10 16:12:39 -0300

    Conceito Boolean e Condicionais

---
+++ js/estruturascondicionais.js
@@ -0,0 +1,16 @@
+// Conceito Boolean & Condicional
+
+const condicionalTrue = true;
+const condicionalFalse = false;
+
+console.log(condicionalTrue + condicionalFalse);
+
+if (condicionalTrue) {
+    console.log("Sim");
+}
+
+if (condicionalFalse) {
+    console.log("Sim");
+} else {
+    console.log("Não");
+}
