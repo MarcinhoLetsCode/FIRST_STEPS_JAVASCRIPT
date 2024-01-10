3584fcb435efee211e5f42cf06038ae17b2e2789
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2024-01-10 15:45:13 -0300

    Desafio Gasto Viagem

---
+++ assets/js/calculovalorviagem.js
@@ -0,0 +1,17 @@
+// Exercício - Cálculo de Valor de Uma Viagem
+// Faça um Programa Para Calcular o Valor De Uma Viagem;
+//
+// Você Terá 3 Variáveis. Sendo Elas:
+// 1 - Preço Do Combustível;
+// 2 - Gasto Médio De Combustível Do Carro Por KM;
+// 3 - Distância Em KM Da Viagem.
+//
+// Imprima No Console o Valor Que Será Gasto Para Realizar Esta Viagem.
+//
+
+let combustivel = 3;
+let gasto = 6;
+let distancia = 12;
+
+console.log("Valor Em Reais: R$ " + (12/6)*3);
+console.log("Valor Em Reais: R$ " + (distancia/gasto)*combustivel);
