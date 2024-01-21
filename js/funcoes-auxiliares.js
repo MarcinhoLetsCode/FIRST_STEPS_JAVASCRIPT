8d57d5bab31b5a8d0310713de25554d105b90ade
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2024-01-21 10:54:20 -0300

    Desvendando_Funcoes_Gets_Print

---
+++ js/funcoes-auxiliares.js
@@ -0,0 +1,18 @@
+
+
+function gets() {
+    return 10;
+}
+
+function print(texto) {
+    console.log(texto);
+}
+
+//module.exports = {
+//    gets: gets, 
+//    print: print
+//};
+//module.exports.gets = gets;
+
+module.exports = {gets, print};
+
---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+//import funcoes from './funcoes-auxiliares';
+//const funcoes = require('./funcoes-auxiliares');
+
+//console.log(funcoes);
+
+//console.log(funcoes.gets());
+//funcoes.print('Hello');
+
+const {gets, print} = require('./funcoes-auxiliares');
+
+print(gets());
